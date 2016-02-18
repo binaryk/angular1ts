7bfdcfde219b4d8b71af67052608629c8fb6dd95
binaryk <lupacescueduard@yahoo.com>
2016-02-18 20:14:49 +0200

    CurrentWork

--- app/app.ts
+++ app/app.ts
@@ -1,1 +1,22 @@
-angular.module("prductManagement",[])
+/// <reference path="../typings/angularjs/angular.d.ts" />
+module app {
+    var main = angular.module("productManagement",["ngRoute","common.services","productResourceMock"]);
+
+    main.config(routeConfig);
+
+    routeConfig.$inject = ["$routeProvider"];
+    function routeConfig($routeProvider):void{
+
+        $routeProvider.when(
+            "/productList",{
+                templateUrl: "/app/products/productListView.html",
+                controller: "ProductListCtrl as vm"
+            })
+            .when(
+            "/productDetail/:productId",{
+                templateUrl: "/app/products/productDetailView.html",
+                controller: "ProductDetailCtrl as vm"
+            }).otherwise("/productList");
+
+    }
+}
